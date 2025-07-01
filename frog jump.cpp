219bb87c07c26b6ee131b98a736000619e210aea
Adi987p <119767109+Adi987p@users.noreply.github.com>
2025-07-01 11:56:25 +0530

    Update frog jump.cpp

--- frog jump.cpp
+++ frog jump.cpp
@@ -82,3 +82,24 @@ public:
         return dp[n-1];
     }
 };
+//Space optimization
+class Solution {
+public:
+    int minCost(vector<int>& height) {
+        int n = height.size();
+        int prev = 0;
+        int prev2 = 0;
+        for (int i = 1; i < n; i++) {
+            int left = prev + abs(height[i] - height[i - 1]);
+            int right = INT_MAX;
+            if(i>1)
+                right = prev2 + abs(height[i] - height[i - 2]);
+            int curri = min(left, right);
+            prev2 = prev;
+            prev = curri;
+        }
+
+        return prev;
+    }
+};
+
