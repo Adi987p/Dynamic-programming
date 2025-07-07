ad47a57c93570bfde5f747c81f3a443178bd0b17
Adi987p <119767109+Adi987p@users.noreply.github.com>
2025-07-07 12:04:01 +0530

    Update Print all LCS sequences.cpp

--- Print all LCS sequences.cpp
+++ Print all LCS sequences.cpp
@@ -58,3 +58,35 @@ class Solution {
     return store;
   }
 };
+
+// space Optimization
+
+class Solution {
+  public:
+    int longestCommonSubstr(string& s1, string& s2) {
+        // your code here
+        int n = s1.size();
+        int m = s2.size();
+        
+        vector<int>prev(m+1,0),curr(m+1,0);
+        
+        int maxi=0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    if(prev[j-1]!=0)
+                        curr[j]=1+prev[j-1];
+                    else
+                        curr[j]=1;
+                }
+                else curr[j]=0;
+                
+                if(maxi<curr[j]) maxi = curr[j];
+            }
+            prev = curr;
+        }
+        
+        return maxi;
+    }
+};
